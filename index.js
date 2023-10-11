d3bd480c128a8b38cd62962547777ceb4570a763
akmalfrhn <141397867+akmalfrhn@users.noreply.github.com>
2023-10-11 15:13:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import db from "./config/database.js";
+import router from "./routes/index.js";
+dotenv.config();
+const app = express();
+
+try{
+    await db.authenticate();
+    console.log('database connected...');
+}catch (error) {
+    console.error(error);
+}
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, ()=> console.log('server running at port 5000'));
